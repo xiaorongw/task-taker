5af867bf9c6a6c3a6eb0c3f31b29fd920b91b444
Xiao Rong W <xr_wong@hotmail.com>
2020-06-15 02:04:08 +0800

    respond to task completion

--- src/components/TodoListComponent.js
+++ src/components/TodoListComponent.js
@@ -4,46 +4,98 @@ import { ListGroup, ListGroupItem, Input } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCalendar, faCircle, faCheckCircle } from '@fortawesome/free-regular-svg-icons';
 
+class TaskItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            completed: false, // to push to redux store
+            hover: false
+        };
+        this.toggleCompleted = this.toggleCompleted.bind(this);
+        this.toggleHover = this.toggleHover.bind(this);
+    }
 
-const RenderTasks = () => {
-    const todolist = TODO.map( (item) => {
-        if (item.due !== '') {
-            return (
-                <div className='row' key={item.id}>
+    toggleCompleted() {
+        this.setState({completed: !this.state.completed});
+    }
+
+    toggleHover() {
+        this.setState({hover: !this.state.hover})
+    }
+
+    render() {
+        if (this.props.item.due) {
+            return(
+                <div className='row' key={this.props.item.id}>
                     <div className='col'>
-                        <span className='change-icon'>
-                            <ListGroupItem className='todo-item'>
-                                <FontAwesomeIcon className="task-icon" icon={faCircle} />
-                                <FontAwesomeIcon className="task-icon" icon={faCheckCircle} />
-                                {item.task}
-                                <br />
-                                <FontAwesomeIcon className="icon" icon={faCalendar} />
-                                <small>{item.due}</small>        
-                            </ListGroupItem>
-                        </span>
+                        <ListGroupItem className='todo-item' 
+                                onClick={this.toggleCompleted}
+                                onMouseOver={this.toggleHover}
+                                onMouseOut={this.toggleHover}
+                        >
+                            {
+                                !this.state.hover ? // if not hovered
+                                    !this.state.completed
+                                        ? <FontAwesomeIcon className="task-icon" icon={faCircle} />
+                                        : <FontAwesomeIcon className="task-icon" icon={faCheckCircle} />
+                                : // if hovered over
+                                    !this.state.completed
+                                        ? <FontAwesomeIcon className="task-icon" icon={faCheckCircle} />
+                                        : <FontAwesomeIcon className="task-icon" icon={faCheckCircle} /> // if already completed, stay checked
+                            }
+                            {
+                                !this.state.completed
+                                    ? <span>{this.props.item.task}</span>
+                                    : <span style={{textDecoration:'line-through'}}>{this.props.item.task}</span>
+                            }
+                            <br />
+                            <FontAwesomeIcon className="icon" icon={faCalendar} />
+                            <small>{this.props.item.due}</small>        
+                        </ListGroupItem>
                     </div>
                 </div>
             );
         }
         else {
-            return (
-                <div className='row'>
+            return(
+                <div className='row' key={this.props.item.id}>
                     <div className='col'>
-                        <span className='change-icon'>
-                            <ListGroupItem className='todo-item'>
-                                <FontAwesomeIcon className="task-icon" icon={faCircle} />
-                                <FontAwesomeIcon className="task-icon" icon={faCheckCircle} />
-                                {item.task}
-                            </ListGroupItem>
-                        </span>
+                        <ListGroupItem className='todo-item' 
+                            onClick={this.toggleCompleted}
+                            onMouseOver={this.toggleHover}
+                            onMouseOut={this.toggleHover}
+                        >
+                            {
+                                !this.state.hover ? // if not hovered
+                                    !this.state.completed
+                                        ? <FontAwesomeIcon className="task-icon" icon={faCircle} />
+                                        : <FontAwesomeIcon className="task-icon" icon={faCheckCircle} />
+                                : // if hovered over
+                                    !this.state.completed
+                                        ? <FontAwesomeIcon className="task-icon" icon={faCheckCircle} />
+                                        : <FontAwesomeIcon className="task-icon" icon={faCheckCircle} /> // if already completed, stay checked
+                            }
+                            {
+                                !this.state.completed
+                                    ? <span>{this.props.item.task}</span>
+                                    : <span style={{textDecoration:'line-through'}}>{this.props.item.task}</span>
+                            }
+                        </ListGroupItem>
                     </div>
                 </div>
             );
         }
+
+    }
+    
+}
+
+const RenderTasks = () => {
+    const todolist = TODO.map( (item) => {
+        return <TaskItem item={item} />
     })
     return todolist;
 }
-
 
 class TodoList extends Component {
     constructor(props) {
